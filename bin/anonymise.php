a82bd00d4c89c254de5a8f5b3eff3c7b30fb46b2
Elliot J. Reed <elliot.reed@bunches.co.uk>
2018-09-25 11:43:03 +0100

    Adds missing dependency

--- bin/anonymise.php
+++ bin/anonymise.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use ElliotJReed\DatabaseAnonymiser\ConfigurationFileParser;
+use ElliotJReed\DatabaseAnonymiser\DatabaseConfiguration;
 use ElliotJReed\DatabaseAnonymiser\DatabaseInformation;
 use ElliotJReed\DatabaseAnonymiser\Validator;
 use ElliotJReed\DatabaseAnonymiser\Anonymiser;
@@ -20,5 +21,5 @@ $connection = $configuration['database-connection'];
 
 $pdo = new PDO($connection['dsn'], $connection['username'], $connection['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-(new Anonymiser($pdo, new Validator(new DatabaseInformation($pdo))))
+(new Anonymiser($pdo, new DatabaseConfiguration($pdo), new Validator(new DatabaseInformation($pdo))))
     ->anonymise($configuration['anonymise']);
