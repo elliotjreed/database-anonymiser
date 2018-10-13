be7b482f2b31647bd4ce347eccc725310a6adf2f
Elliot J. Reed <elliot.reed@bunches.co.uk>
2018-10-13 21:05:49 +0100

    Simplifies MySQL foreign key check test

--- tests/ElliotJReed/DatabaseAnonymiser/MysqlDatabaseConfigurationTest.php
+++ tests/ElliotJReed/DatabaseAnonymiser/MysqlDatabaseConfigurationTest.php
@@ -4,54 +4,19 @@ declare(strict_types=1);
 namespace ElliotJReed\Tests\DatabaseAnonymiser;
 
 use ElliotJReed\DatabaseAnonymiser\DatabaseConfiguration;
-use PDOException;
 
 class MysqlDatabaseConfigurationTest extends DatabaseTestCase
 {
     /**
      * @return void
      */
-    public function tearDown(): void
-    {
-        $this->pdo->exec(
-            'DROP TABLE example_table;
-          DROP TABLE example_second_table'
-        );
-    }
-
-    /**
-     * @return void
-     */
-    public function testItThrowsExceptionByDefaultIfForeignKeyChecksAreEnabled(): void
-    {
-        $this->expectException(PDOException::class);
-
-        $this->pdo->exec(
-            'SET FOREIGN_KEY_CHECKS=1;
-            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY) ENGINE=INNODB;
-            INSERT INTO example_table (example_column) VALUES (1);
-            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), INDEX ref_index (ref_column), FOREIGN KEY(ref_column) REFERENCES example_table(example_column))) ENGINE=INNODB;
-            INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);
-            DROP TABLE example_table;'
-        );
-    }
-
-    /**
-     * @return void
-     */
     public function testItTurnsOffForeignKeyChecks(): void
     {
-        $this->pdo->exec(
-            'SET FOREIGN_KEY_CHECKS=1;
-            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY) ENGINE=INNODB;
-            INSERT INTO example_table (example_column) VALUES (1);
-            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), INDEX ref_index (ref_column), FOREIGN KEY(ref_column) REFERENCES example_table(example_column))) ENGINE=INNODB;
-            INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);'
-        );
-
+        $this->pdo->exec('SET FOREIGN_KEY_CHECKS=ON');
         (new DatabaseConfiguration($this->pdo))->disableForeignKeyChecks();
-        $rowsAffected = $this->pdo->exec('DROP TABLE example_table');
 
-        $this->assertSame(1, $rowsAffected);
+        $query = $this->pdo->query('SHOW LOCAL VARIABLES LIKE "foreign_key_checks"');
+
+        $this->assertSame('OFF', $query->fetch()['Value']);
     }
 }
