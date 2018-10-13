770beb1ccca87fad81f8995eee1a1287231dfc78
Elliot J. Reed <elliot.reed@bunches.co.uk>
2018-10-13 20:53:42 +0100

    Create parent index for foreign key constraint

--- tests/ElliotJReed/DatabaseAnonymiser/MysqlDatabaseConfigurationTest.php
+++ tests/ElliotJReed/DatabaseAnonymiser/MysqlDatabaseConfigurationTest.php
@@ -28,9 +28,9 @@ class MysqlDatabaseConfigurationTest extends DatabaseTestCase
 
         $this->pdo->exec(
             'SET FOREIGN_KEY_CHECKS=1;
-            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY);
+            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY) ENGINE=INNODB;
             INSERT INTO example_table (example_column) VALUES (1);
-            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), FOREIGN KEY(ref_column) REFERENCES example_table(example_column));
+            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), INDEX ref_index (ref_column), FOREIGN KEY(ref_column) REFERENCES example_table(example_column))) ENGINE=INNODB;
             INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);
             DROP TABLE example_table;'
         );
@@ -43,9 +43,9 @@ class MysqlDatabaseConfigurationTest extends DatabaseTestCase
     {
         $this->pdo->exec(
             'SET FOREIGN_KEY_CHECKS=1;
-            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY);
+            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY) ENGINE=INNODB;
             INSERT INTO example_table (example_column) VALUES (1);
-            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), FOREIGN KEY(ref_column) REFERENCES example_table(example_column));
+            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), INDEX ref_index (ref_column), FOREIGN KEY(ref_column) REFERENCES example_table(example_column))) ENGINE=INNODB;
             INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);'
         );
 
