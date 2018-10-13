86f21b081b83486a0cbe860f6702d1edad0b1711
Elliot J. Reed <elliot.reed@bunches.co.uk>
2018-10-13 20:49:14 +0100

    Moves tearDown method to top of class

--- tests/ElliotJReed/DatabaseAnonymiser/MysqlDatabaseConfigurationTest.php
+++ tests/ElliotJReed/DatabaseAnonymiser/MysqlDatabaseConfigurationTest.php
@@ -11,25 +11,6 @@ class MysqlDatabaseConfigurationTest extends DatabaseTestCase
     /**
      * @return void
      */
-    public function testItTurnsOffForeignKeyChecks(): void
-    {
-        $this->pdo->exec(
-            'SET FOREIGN_KEY_CHECKS=1;
-            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY);
-            INSERT INTO example_table (example_column) VALUES (1);
-            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), FOREIGN KEY(ref_column) REFERENCES example_table(example_column));
-            INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);'
-        );
-
-        (new DatabaseConfiguration($this->pdo))->disableForeignKeyChecks();
-        $rowsAffected = $this->pdo->exec('DROP TABLE example_table');
-
-        $this->assertSame(1, $rowsAffected);
-    }
-
-    /**
-     * @return void
-     */
     public function tearDown(): void
     {
         $this->pdo->exec(
@@ -53,5 +34,24 @@ class MysqlDatabaseConfigurationTest extends DatabaseTestCase
             INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);
             DROP TABLE example_table;'
         );
+    }
+
+    /**
+     * @return void
+     */
+    public function testItTurnsOffForeignKeyChecks(): void
+    {
+        $this->pdo->exec(
+            'SET FOREIGN_KEY_CHECKS=1;
+            CREATE TABLE example_table (example_column INT(1) PRIMARY KEY);
+            INSERT INTO example_table (example_column) VALUES (1);
+            CREATE TABLE example_second_table (example_column INT(1), ref_column INT(1), FOREIGN KEY(ref_column) REFERENCES example_table(example_column));
+            INSERT INTO example_second_table (example_column, ref_column) VALUES (1, 1);'
+        );
+
+        (new DatabaseConfiguration($this->pdo))->disableForeignKeyChecks();
+        $rowsAffected = $this->pdo->exec('DROP TABLE example_table');
+
+        $this->assertSame(1, $rowsAffected);
     }
 }
